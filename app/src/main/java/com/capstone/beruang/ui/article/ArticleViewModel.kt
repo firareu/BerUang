aa851f26e4660c97f32b6682ce8aa261a5d4b5cb
Putri Dian S <91788765+firareu@users.noreply.github.com>
2023-11-19 15:45:52 +0700

    new bottom nav bar

--- app/src/main/java/com/capstone/beruang/ui/article/ArticleViewModel.kt
+++ app/src/main/java/com/capstone/beruang/ui/article/ArticleViewModel.kt
@@ -1,13 +1,13 @@
-package com.capstone.beruang.ui.notifications
+package com.capstone.beruang.ui.article
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ArticleViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is article Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/com/capstone/beruang/ui/management/ManagementFragment.kt
+++ app/src/main/java/com/capstone/beruang/ui/management/ManagementFragment.kt
@@ -1,4 +1,4 @@
-package com.capstone.beruang.ui.notifications
+package com.capstone.beruang.ui.management
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.capstone.beruang.databinding.FragmentNotificationsBinding
+import com.capstone.beruang.databinding.FragmentManagementBinding
 
-class NotificationsFragment : Fragment() {
+class ManagementFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentManagementBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val managementViewModel =
+            ViewModelProvider(this).get(ManagementViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentManagementBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textManagement
+        managementViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
