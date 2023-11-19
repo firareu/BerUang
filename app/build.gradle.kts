b2f5b45c36170abb7c80e79bb0140e74beb7da14
Putri Dian S <91788765+firareu@users.noreply.github.com>
2023-11-19 23:10:02 +0700

    piechart n font

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("com.github.furkanaskin:ClickablePieChart:1.0.9")
+
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
 }
--- app/src/main/java/com/capstone/beruang/MainActivity.kt
+++ app/src/main/java/com/capstone/beruang/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.capstone.beruang
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -13,17 +15,22 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    /*lateinit var goBarChart: Button
+
+    lateinit var goPieChart:Button
+
+    lateinit var goRadarChart:Button*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Atur toolbar sebagai ActionBar
+        setSupportActionBar(binding.toolbar)
 
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_article, R.id.navigation_management, R.id.navigation_profile
@@ -31,5 +38,29 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        /*goBarChart=findViewById(R.id.go_bar_chart)
+
+        goPieChart=findViewById(R.id.go_pie_chart)
+
+        goRadarChart=findViewById(R.id.go_radar_chart)
+
+        *//*goBarChart.setOnClickListener {
+
+            startActivity(Intent(this,BarChartActivity::class.java))
+
+        }*//*
+
+        goPieChart.setOnClickListener {
+
+            startActivity(Intent(this,PieChartActivity::class.java))
+
+        }
+
+        *//*goRadarChart.setOnClickListener {
+
+            startActivity(Intent(this,RadarChartActivity::class.java))
+
+        }*/
     }
 }
