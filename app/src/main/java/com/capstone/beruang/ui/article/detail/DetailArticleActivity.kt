3eb24458f815feed2b33555afda32cb74143c517
Aufa <aufa0987654321@gmaill.com>
2023-12-17 17:40:41 +0700

    Add appbar

--- app/src/main/java/com/capstone/beruang/ui/article/detail/DetailArticleActivity.kt
+++ app/src/main/java/com/capstone/beruang/ui/article/detail/DetailArticleActivity.kt
@@ -6,8 +6,8 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.capstone.beruang.R
-import com.capstone.beruang.databinding.ActivityDetailArticleBinding
 import com.capstone.beruang.data.Article
+import com.capstone.beruang.databinding.ActivityDetailArticleBinding
 
 class DetailArticleActivity : AppCompatActivity() {
 
@@ -23,6 +23,7 @@ class DetailArticleActivity : AppCompatActivity() {
         binding = ActivityDetailArticleBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.topAppBar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val article = intent.getParcelableExtra<Article>(ARTICLE_KEY)
