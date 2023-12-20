33b62f7518cc33fb9656ee0466e707a57b60a1bd
Putri Dian S <91788765+firareu@users.noreply.github.com>
2023-12-20 19:27:31 +0700

    up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,15 +43,14 @@ android {
 }
 
 dependencies {
-//    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.12.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
     implementation("com.google.firebase:firebase-auth:22.3.0")
     testImplementation("junit:junit:4.13.2")
@@ -63,6 +62,7 @@ dependencies {
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
     implementation ("com.akexorcist:round-corner-progress-bar:2.2.1")
     implementation ("com.airbnb.android:lottie:3.6.1")
+
 
     //for image
     implementation ("de.hdodenhof:circleimageview:3.1.0")
