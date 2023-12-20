167dc6f934cb5696a9b181ecac80a3d417cab659
LynnArsa <lintang13arsa@gmail.com>
2023-12-21 03:08:31 +0700

    fix typo(?)

--- src/controllers/allocationController.js
+++ src/controllers/allocationController.js
@@ -26,7 +26,7 @@ const {
       if (allocation.length > 0) {
         res.status(200).json({ allocation });
       } else {
-        res.status(404).json({ error: "allocation not found" });
+        res.status(404).json({ error: "Allocation not found" });
       }
     } catch (error) {
       console.error(error);
--- src/controllers/outcomeController.js
+++ src/controllers/outcomeController.js
@@ -1,83 +1,89 @@
-const { addOutcome: addOutcomeModel,
-    getOutcome: getOutcomeModel,
-    getOutcomeById: getOutcomeByIdModel,
-    updateOutcome: updateOutcomeModel,
-    deleteOutcome: deleteOutcomeModel } = require('../models/outcome');
+const {
+  addOutcome: addOutcomeModel,
+  getOutcome: getOutcomeModel,
+  getOutcomeById: getOutcomeByIdModel,
+  updateOutcome: updateOutcomeModel,
+  deleteOutcome: deleteOutcomeModel,
+} = require("../models/outcome");
 
 // POST
 const addOutcome = async (req, res) => {
-try {
-const outcomeId = await addOutcomeModel(req.body);
-console.log('Outcome added successfully:', outcomeId);
-res.status(201).json({outcomeId});
-} catch (error) {
-console.error('Error creating adding outcome.', error);
-res.status(500).json({error: 'Internal Server Error'});
-}
+  try {
+    const outcomeId = await addOutcomeModel(req.body);
+    console.log("Outcome added successfully:", outcomeId);
+    res.status(201).json({ outcomeId });
+  } catch (error) {
+    console.error("Error creating adding outcome.", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // GET ALL
 const getOutcome = async (req, res) => {
-try {
-const userId = req.body.userId
-const outcome = await getOutcomeModel(userId);
-if (outcome.length > 0) {
-  res.status(200).json({ outcome });
-} else {
-  res.status(404).json({ error: 'Outcome not found' });
-}
-} catch (error) {
-console.error(error);
-res.status(500).json({ error: 'Internal Server Error '});
-}
+  try {
+    const userId = req.body.userId;
+    const outcome = await getOutcomeModel(userId);
+    if (outcome.length > 0) {
+      res.status(200).json({ outcome });
+    } else {
+      res.status(404).json({ error: "Outcome not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error " });
+  }
 };
 
 // GET by Id
 const getOutcomeById = async (req, res) => {
-try {
-const outcomeId = req.params.outcomeId;
-if (!outcomeId) {
-  return res.status(400).json({ error: 'Outcome ID is missing in the request' });
-}
+  try {
+    const outcomeId = req.params.outcomeId;
+    if (!outcomeId) {
+      return res
+        .status(400)
+        .json({ error: "Outcome ID is missing in the request" });
+    }
 
-const outcome = await getOutcomeByIdModel(outcomeId);
-if (outcome) {
-  res.status(200).json({ outcome });
-} else {
-  res.status(404).json({ error: 'Outcome not found'});
-}
-} catch (error) {
-console.error(error);
-res.status(500).json({ error: 'Internal Server Error' })
-}
+    const outcome = await getOutcomeByIdModel(outcomeId);
+    if (outcome) {
+      res.status(200).json({ outcome });
+    } else {
+      res.status(404).json({ error: "Outcome not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // PUT
 const updateOutcome = async (req, res) => {
-try {
-const outcomeId = req.params.outcomeId;
-await updateOutcomeModel(outcomeId, req.body);
-res.status(200).json({ message: 'Outcome updated successfully!' });
-} catch (error) {
-console.error(error);
-res.status(500).json({ error: 'Internal Server Error' });
-}
+  try {
+    const outcomeId = req.params.outcomeId;
+    await updateOutcomeModel(outcomeId, req.body);
+    res.status(200).json({ message: "Outcome updated successfully!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // DELETE
 const deleteOutcome = async (req, res) => {
-try {
-const outcomeId = req.params.outcomeId;
-await deleteOutcomeModel(outcomeId);
-res.status(200).json({ message: 'Outcome deleted successfully' });
-} catch (error) {
-console.error(error);
-res.status(500).json({ error : 'Internal Server Error' });
-}
+  try {
+    const outcomeId = req.params.outcomeId;
+    await deleteOutcomeModel(outcomeId);
+    res.status(200).json({ message: "Outcome deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
-module.exports = { addOutcome, 
-               getOutcome,  
-               getOutcomeById, 
-               updateOutcome,
-               deleteOutcome };
+module.exports = {
+  addOutcome,
+  getOutcome,
+  getOutcomeById,
+  updateOutcome,
+  deleteOutcome,
+};
